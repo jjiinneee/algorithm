67481bda3e5d787021014e38643a888143b47f1b
jjiinneee <llj2653@naver.com>
2022-03-26 23:27:15 +0900

    문자열 알고리즘

---
+++ src/com/algorithm/strEx2.java
@@ -0,0 +1,36 @@
+package com.algorithm;
+
+import java.util.Scanner;
+
+public class strEx2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int[] arr = new int[26];
+
+        for(int i=0; i<arr.length;i++){
+            arr[i] = -1;
+        }
+        String input = sc.nextLine();
+
+        for(int i=0; i<input.length();i++){
+            char ch = input.charAt(i);
+
+            if(arr[ch - 'a'] == -1){
+                arr[ch - 'a'] = i;
+            }
+        }
+
+//        for(int i=0; i<arr.length; i++){
+//            System.out.println(arr[i] + " ");
+//        }
+        for(int val : arr) {
+            System.out.print(val + " ");
+        }
+    }
+
+
+
+}
---
+++ src/com/algorithm/strEx3.java
@@ -0,0 +1,21 @@
+package com.algorithm;
+
+import java.util.Scanner;
+
+public class strEx3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = scanner.nextInt();
+        int number2 =  scanner.nextInt();
+
+        number1 = Integer.parseInt(new StringBuilder().append(number1).reverse().toString());
+        number2 = Integer.parseInt(new StringBuilder().append(number2).reverse().toString());
+
+       if(number1 > number2){
+           System.out.println(number1);
+       }else{
+           System.out.println(number2);
+       }
+    }
+}
