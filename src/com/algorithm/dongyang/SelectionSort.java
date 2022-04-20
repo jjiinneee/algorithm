f4167770bd2635e90d9aebb78087d120ad9b7ae5
jjiinneee <llj2653@gmail.com>
2022-04-20 23:52:16 +0900

    selectsort

---
+++ src/com/algorithm/dongyang/SelectionSort.java
@@ -0,0 +1,10 @@
+package com.algorithm.dongyang;
+
+public class SelectionSort {
+  public static void printArray(int[] a){
+    for (int i = 0; i < a.length; i++) {
+      System.out.println("[" + a[i] + "]");
+    }
+    System.out.printf("\n");
+  }
+}
