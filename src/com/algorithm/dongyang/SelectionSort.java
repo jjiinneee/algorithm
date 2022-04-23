d6727e1435cd17d0522fc2f3701e6fef923a9304
jjiinneee <llj2653@gmail.com>
2022-04-23 23:48:37 +0900

    selectionSort

--- src/com/algorithm/dongyang/SelectionSort.java
+++ src/com/algorithm/dongyang/SelectionSort.java
@@ -7,4 +7,26 @@ public class SelectionSort {
     }
     System.out.printf("\n");
   }
+  
+  public static void main(String[] args) {
+    int[] a ={90,34,78,12,56};
+    System.out.println("정렬 전 배열");
+    printArray(a);
+    
+    int  min,  temp;
+    System.out.println(a.length);
+    for (int i = 0; i < a.length; i++) {
+        min = i;
+      for (int j = i+1; j < a.length; j++) {
+        if(a[j] < a[i]){
+          min = j;
+          temp = a[i];
+          a[i] = a[min];
+          a[min] = temp;
+        }
+      }
+    }
+    System.out.println("정렬 후 배열");
+    printArray(a);
+  }
 }
