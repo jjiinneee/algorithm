8ff40f768659d29c553354357ec2bdd36ce7eab1
jjiinneee <llj2653@gmail.com>
2022-05-03 19:54:04 +0900

    clock

---
+++ src/com/algorithm/hanbit/Clock.java
@@ -0,0 +1,23 @@
+package com.algorithm.hanbit;
+
+import java.util.Scanner;
+
+public class Clock {
+  public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    
+    int time = sc.nextInt();
+    int cnt = 0;
+  
+    for (int i = 0; i <= time; i++) {
+      for (int j = 0; j < 60; j++) {
+        for (int k = 0; k < 60; k++) {
+//          if(time)
+
+        }
+      }
+    }
+  }
+  
+
+}
