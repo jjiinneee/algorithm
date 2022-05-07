df5ee4a9a3bac89722830fcacc6dfed067288bb7
jjiinneee <llj2653@gmail.com>
2022-05-07 23:18:17 +0900

    backjoon

---
+++ src/com/algorithm/backjoon/Back10808.java
@@ -0,0 +1,16 @@
+package com.algorithm.backjoon;
+
+import java.util.Scanner;
+
+public class Back10808 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String word = sc.nextLine();
+    
+    int[] alphabet = new int[26];
+  
+    for (int i = 0; i < word.length(); i++) {
+      
+    }
+  }
+}
---
+++ src/com/algorithm/backjoon/Back2743.java
@@ -0,0 +1,12 @@
+package com.algorithm.backjoon;
+
+import java.util.Scanner;
+
+public class Back2743 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    String word = sc.nextLine();
+    System.out.println(word.length());
+  }
+}
