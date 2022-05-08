b95e6446386eb3ec81f6916ce7387e33f2094735
jjiinneee <llj2653@gmail.com>
2022-05-08 23:37:14 +0900

    back

--- src/com/algorithm/backjoon/Back10808.java
+++ src/com/algorithm/backjoon/Back10808.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class Back10808 {
   public static void main(String[] args) {
+//    String test = "Hello";
+//    System.out.println(test.charAt(0));
     Scanner sc = new Scanner(System.in);
     String word = sc.nextLine();
-    
+
     int[] alphabet = new int[26];
-  
+
     for (int i = 0; i < word.length(); i++) {
-      
+      alphabet[word.charAt(i) -97]++;
+    }
+
+    for (int i = 0; i < alphabet.length; i++) {
+      System.out.print(alphabet[i] +" ");
     }
   }
 }
