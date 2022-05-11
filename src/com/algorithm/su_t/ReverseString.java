08ed5e9c814bc29d3606318c9668abc15b5842f9
jjiinneee <llj2653@gmail.com>
2022-05-11 10:40:03 +0900

    ReverseString

---
+++ src/com/algorithm/su_t/ReverseString.java
@@ -0,0 +1,28 @@
+package com.algorithm.su_t;
+
+import java.util.Arrays;
+
+// https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1440/
+public class ReverseString {
+  public void reverseString(char[] s) {
+    int length = s.length;
+//    for (int i = 0, j = length - 1; i < j; i++, j--) {
+//      char temp = s[i];
+//      s[i] = s[j];
+//      s[j] = temp;
+//    }
+    for (int i = 0; i < length / 2; i++) {
+      int j = length - 1 - i;
+      char temp = s[i];
+      s[i] = s[j];
+      s[j] = temp;
+    }
+  }
+  
+  public static void main(String[] args) {
+    char[] arr = {48, 49, 50, 51, 52, 53, 54, 55};
+    ReverseString s = new ReverseString();
+    s.reverseString(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+}
