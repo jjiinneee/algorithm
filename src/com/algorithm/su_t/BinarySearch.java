e66258c02e43dac50f1cccd26593f6a0697337f0
jjiinneee <llj2653@gmail.com>
2022-05-21 03:06:41 +0900

    searchmatrix

--- src/com/algorithm/su_t/BinarySearch.java
+++ src/com/algorithm/su_t/BinarySearch.java
@@ -1,20 +1,48 @@
 package com.algorithm.su_t;
 
 public class BinarySearch {
+//  public int search(int[] nums, int target) {
+//    for (int i = 0; i < nums.length; i++) {
+//      if(nums[i] == target) {
+//        return i;
+//      }
+//    }
+//    return -1;
+//  }
+  
+  
+  //이진탐색.. 가운데먼저 찾고
+  //target이랑 같으면 인덱스값 출력
+  //아니면 계속 돌아
+  //nums.length >>  배열의 개수
+  //nums[nums.length -1]  >> 배열의 마지막 요소
   public int search(int[] nums, int target) {
-    for (int i = 0; i < nums.length; i++) {
-      if(nums[i] == target) {
-        return i;
+  
+    int start  = 0;
+    int end = nums.length;
+    int mid = (start + end) / 2;
+  
+    while(start < end){
+      if(nums[mid]< target){
+        start = mid + 1;
       }
+      else if(nums[mid] > target){
+        end = mid;
+      }else{
+        return mid;
+      }
+      mid = (start + end) / 2;
     }
+    
+
     return -1;
   }
   
   public static void main(String[] args) {
      int[] nums = {-1,0,3,5,9,12};
-//     int target = 9;
-    int target = 2;
-  
+     int target = 9;
+  //  int target = 2;
+  //  System.out.println(nums[nums.length/2]);
     BinarySearch s = new BinarySearch();
     s.search(nums, target);
     System.out.println(s.search(nums,target));
---
+++ src/com/algorithm/su_t/SearchMatrix.java
@@ -0,0 +1,55 @@
+package com.algorithm.su_t;
+
+public class SearchMatrix {
+//  public boolean searchMatrix(int[][] matrix, int target) {
+//
+//    for (int i = 0; i < matrix.length; i++) {
+//      for (int j = 0; j < matrix[i].length; j++) {
+//        //3 2  target = 3
+//        if(matrix[i][j] == target){
+//          return true;
+//        }
+//      }
+//    }
+//
+//    return false;
+//  }
+  
+  public boolean searchMatrix(int[][] matrix, int target) {
+    
+   int[] arr = new int[matrix.length * matrix[0].length];
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        arr[matrix[i].length * i +j] = matrix[i][j];
+      }
+    }
+    
+    int start = 0;
+    int end = arr.length;
+    int mid = (start + end) /2;
+    
+    
+    while(start < end){
+      if(arr[mid] <target){
+        start  = mid +1;
+      }
+      else if(arr[mid] > target){
+        end = mid;
+      }else{
+        return true;
+      }
+      mid = (start + end ) /2;
+    }
+    
+    return false;
+  }
+  
+  public static void main(String[] args) {
+   int[][]  matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+   int target = 5;
+   
+   SearchMatrix s = new SearchMatrix();
+   s.searchMatrix(matrix,target);
+    System.out.println(s.searchMatrix(matrix,target));
+  }
+}
