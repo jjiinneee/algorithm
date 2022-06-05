b86c5c0e8036490f8593fdf3973f34fd3e1874e6
jjiinneee <llj2653@gmail.com>
2022-06-05 21:13:49 +0900

    back11047

---
+++ src/com/algorithm/backjoon/Back11047.java
@@ -0,0 +1,27 @@
+package com.algorithm.backjoon;
+
+import java.util.Scanner;
+
+public class Back11047 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    int n = sc.nextInt();
+    int  k = sc.nextInt();
+    int count = 0;
+    
+    int[] coin = new int[n];
+  
+    for (int i = 0; i < n; i++) {
+     coin[i] = sc.nextInt();
+    }
+  
+    for(int i = n - 1; i >= 0; i--) {
+      if(coin[i] <= k) {
+        count += (k / coin[i]);
+        k = k % coin[i];
+      }
+    }
+    System.out.println(count);
+  }
+}
