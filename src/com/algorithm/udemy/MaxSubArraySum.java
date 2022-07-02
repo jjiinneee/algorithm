cd066ec1f5f3844562b9f52a7665104651f5bbb5
jjiinneee <llj2653@gmail.com>
2022-07-03 00:09:32 +0900

    modify_MaxSubArraySum

--- src/com/algorithm/udemy/MaxSubArraySum.java
+++ src/com/algorithm/udemy/MaxSubArraySum.java
@@ -11,12 +11,13 @@ public class MaxSubArraySum {
     
     for (int i=0; i<k; i++){
       maxSum += nums[i];
+    //  System.out.println(maxSum);
     }
     
     tempSum = maxSum;
     
     for (int i = k; i < nums.length; i++) {
-      tempSum = tempSum - nums[i -k] + nums[i];
+      tempSum +=  nums[i] - nums[i -k];
       maxSum = Math.max(maxSum,tempSum);
     }
     
@@ -25,8 +26,10 @@ public class MaxSubArraySum {
   
   public static void main(String[] args) {
     MaxSubArraySum m = new MaxSubArraySum();
-    int[] nums = {2,6,9,2,1,8,5,6,3};
-    int k = 3;
+//    int[] nums = {2,6,9,2,1,8,5,6,3};
+//    int k = 3;
+    int[] nums = {100,200,300,400};
+    int k = 2;
     System.out.println(m.maxSum(nums, k));
   }
 }
