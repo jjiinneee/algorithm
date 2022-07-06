f5cb2092fb72424444e4617ecb4b5e64c0ea0690
jjiinneee <llj2653@gmail.com>
2022-07-06 23:40:29 +0900

    MatrixTest

---
+++ src/com/algorithm/grammar/ArrayMatrix.java
@@ -0,0 +1,16 @@
+package com.algorithm.grammar;
+
+public class ArrayMatrix {
+  public static void main(String[] args) {
+    int[][] arr = new int[4][4];
+    int num = 1;
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i].length; j++) {
+        arr[i][j] = num;
+        System.out.print(arr[i][j]);
+        num++;
+      }
+      System.out.println();
+    }
+  }
+}
--- src/com/algorithm/programers/CodingTest2.java
+++ src/com/algorithm/programers/CodingTest2.java
@@ -5,19 +5,40 @@ public class CodingTest2 {
 
     int[][] answer= new int[n][n];
   
-
+    int start = 1;
     for (int i = 0; i < answer.length ; i++) {
         for (int j = 0; j < answer[i].length; j++) {
-          System.out.println("i" + answer[i][j++]);
+          
+          answer[i][j] = start;
+          if(horizontal ==true){
+          
+          }
+          System.out.print( answer[i][j]);
+         
 //          if(horizontal == true){
 //            answer[n][n] = answer[i][j];
 //          }
         }
+      System.out.println();
     }
     
     
     return answer;
   }
+  
+  
+  // true
+  // 1   2   9   10
+//   4   3   8   11
+//   5   6   7   12
+//   16  15  14  13
+
+  //false
+  //  1   4   5    16  17
+  //  2   3   6    15  18
+  //  9   8   7    14  19
+  //  10  11  12   13  20
+  //  25  24  23   22  21
   
   public static void main(String[] args) {
     CodingTest2 c = new CodingTest2();
