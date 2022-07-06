14c20ec54a0f14597c6ad114b95fd223b424500d
jjiinneee <llj2653@gmail.com>
2022-07-06 23:20:17 +0900

    modifyStringCompression

--- src/com/algorithm/programers/StringCompression.java
+++ src/com/algorithm/programers/StringCompression.java
@@ -11,15 +11,39 @@ public class StringCompression {
       String cur = "";
       int cnt =1;
       StringBuilder sb = new StringBuilder();
+  
+      for (int start = i; start <= s.length(); start+=i) {
+        if(start + i >= s.length()){
+          cur = s.substring(start, s.length());
+        }else{
+          cur = s.substring(start, start+i);
+        }
+        
+        if(cur.equals(target)){
+          cnt++;
+        }else if(cnt == 1){
+          sb.append(target);
+          target = cur;
+        }else{
+          sb.append(cnt).append(target);
+          target = cur;
+          cnt = 1;
+        }
+      }
+      
+      if(i != target.length()){
+        sb.append(target);
+        answer = Math.min(answer, sb.toString().length());
+      }
     }
-    
-    
+  
+  
     return answer;
   }
   
   public static void main(String[] args) {
     StringCompression s = new StringCompression();
-    String str = "aabbaccc";
+    String str = "xababcdcdababcdcd";
     System.out.println(s.solution(str));
   }
 }
