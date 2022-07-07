f6ff95e4d4873e069914d9eb2e97a840ef0104f9
jjiinneee <llj2653@gmail.com>
2022-07-07 22:36:55 +0900

    ReArrange

---
+++ src/com/algorithm/hanbit/StringReArrange.java
@@ -0,0 +1,28 @@
+package com.algorithm.hanbit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class StringReArrange {
+  
+  public String ReArragement(String str){
+  
+    ArrayList<Character> list = new ArrayList<>();
+    int value = 0;
+    for (int i = 0; i < str.length(); i++) {
+     if(Character.isLetter(str.charAt(i))){
+       list.add(str.charAt(i));
+     }else{
+       value += str.charAt(i) - '0';
+     }
+    }
+    Collections.sort(list);
+    return String.valueOf(list);
+  }
+  
+  public static void main(String[] args) {
+    StringReArrange s = new StringReArrange();
+    String str = "K1KA5CB7";
+    System.out.println(s.ReArragement(str));
+  }
+}
