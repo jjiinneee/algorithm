55f92fc82a7c88de75782a9d60bcacc6d8f8b9da
jjiinneee <llj2653@gmail.com>
2022-07-08 23:25:26 +0900

    dfsEx3

--- src/com/algorithm/grammar/DFSEx3.java
+++ src/com/algorithm/grammar/DFSEx3.java
@@ -1,5 +1,27 @@
 package com.algorithm.grammar;
 
-public class DFSEx3 {
+import java.util.ArrayList;
 
+public class DFSEx3 {
+  
+  public static boolean[] vistied = new boolean[9];
+  public static ArrayList<ArrayList<Integer>>  graph = new ArrayList<ArrayList<Integer>>();
+  
+  public static void dfs(int x){
+    vistied[x] = true;
+    System.out.println(x + " ");
+    for (int i = 0; i < graph.get(x).size(); i++) {
+      int y = graph.get(x).get(i);
+      if(!vistied[y]){
+        dfs(y);
+      }
+    }
+  }
+  public static void main(String[] args) {
+    for (int i = 0; i < 9; i++) {
+      graph.add(new ArrayList<Integer>());
+    }
+    
+    
+  }
 }
