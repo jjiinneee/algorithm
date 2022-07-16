bd35f41f1a176d89859f001b3468411c0d10a441
jjiinneee <llj2653@gmail.com>
2022-07-16 21:40:37 +0900

    NumberToZero

---
+++ src/com/algorithm/leetcode/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,27 @@
+package com.algorithm.leetcode;
+
+public class NumberOfStepsToReduceANumberToZero {
+  
+  
+  public int ReduceNumber(int num){
+    
+    int step = 0;
+  
+    while(num > 0){
+      if(num % 2 == 0){
+        num = num/2;
+        step++;
+      }else {
+        num -= 1;
+        step++;
+      }
+    }
+   
+    return step;
+  }
+  public static void main(String[] args) {
+    NumberOfStepsToReduceANumberToZero n = new NumberOfStepsToReduceANumberToZero();
+    int num = 8;
+    System.out.println(n.ReduceNumber(num));
+  }
+}
